c31d03383a091bb7e5b18806f047ac930f90bc4e
iabosi <russ.sherman@gmail.com>
2015-10-31 14:26:18 -0700

    you need user agent for this to return 200

--- reddit_py/comments.py
+++ reddit_py/comments.py
@@ -1,7 +1,6 @@
 __author__ = 'idclark'
 import requests as r
 
-#TODO this returns 404
 def comments_from_article(subreddit, article, sort):
     """
     return the comments for a given subreddit and article
@@ -15,6 +14,7 @@ def comments_from_article(subreddit, article, sort):
         raise Exception('Please select a valid sort criteria')
     url = r'http://www.reddit.com/r/{s}/comments/{a}.json'.format(s=subreddit, a=article)
     data = {'sort': sort}
+    headers = {'User-Agent' : 'reddit_py comments'}
     response = r.get(url, data=data)
     return response.json()
 
